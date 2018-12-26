268aef487b5f615c660ee61e606c524275d9a6d8
Anton Rudnev <anton.rudnev@gmail.com>
2018-12-26 01:45:04 -0500

    Geocoding is implemented.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import re
 import json
+import time
+from urllib.parse import quote
 import urllib.request
-from flask import Flask, redirect, request, render_template
+from flask import Flask, jsonify, redirect, request, render_template
 
 from settings import MAP_SERVER_HOST, TRIP_SERVER_HOST, TEST_POINTS
 
@@ -31,19 +33,29 @@ def input_points():
                       for k, v in request.form.to_dict().items() if k.startswith('point') and v]
         if request.form['action'] == 'view':
             view_input_url = build_map_url(MAP_SERVER_HOST, geo_points)
-            print(view_input_url)
             return redirect(view_input_url)
         elif request.form['action'] == 'optimize':
             trip_url = build_trip_url(TRIP_SERVER_HOST, geo_points)
-            print(trip_url)
             with urllib.request.urlopen(trip_url) as response:
                 way_points = json.loads(response.read())['waypoints']
                 trip_points = [{'index': i, 'order': w['waypoint_index']} for i, w in enumerate(way_points)]
                 sorted_geo_points = [geo_points[p['index']] for p in sorted(trip_points, key=lambda x: x['order'])]
                 view_trip_url = build_map_url(MAP_SERVER_HOST, sorted_geo_points)
                 return redirect(view_trip_url)
-
     return render_template('input_points.html', geo_points=TEST_POINTS)
+
+
+def search_nominatim(query):
+    url = f'https://nominatim.openstreetmap.org/search/{quote(query)}?limit=1&format=json'
+    with urllib.request.urlopen(url) as response:
+        geo = json.loads(response.read())
+        return geo
+
+
+@app.route('/search/<query>', methods=['GET'])
+def geocoding(query):
+    geo = search_nominatim(query)[0]
+    return jsonify({"lat": geo["lat"], "lon": geo["lon"], "display_name": geo["display_name"]})
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 MAP_SERVER_HOST = 'http://54.175.11.175:9966'
 TRIP_SERVER_HOST = 'http://54.175.11.175:5050'
 
-TEST_POINTS = ['38.902815 -77.060595',
-               '38.946460 -76.996822',
-               '38.922024 -77.055359',
-               '38.911339 -77.007809',
-               '38.882214 -76.998539',
-               '38.935845 -77.029696',
-               '38.886757 -77.021027',
-               '38.922024 -76.963348',
-               '38.907348 -77.059972']
+TEST_POINTS = [{'name': '1000, Thomas Jefferson St NW, Georgetown, Washington, D.C.', 'geolocation': '38.902815 -77.060595'},
+               {'name': '4703, 7th Street Northeast, Michigan Park, Washington, D.C.', 'geolocation': '38.946460 -76.996822'},
+               {'name': '2801, McGill Terrace Northwest, Woodland-Normanstone, Washington, D.C.', 'geolocation': '38.922024 -77.055359'},
+               {'name': '26, Q Street Northeast, Eckington, Chinatown, Washington, D.C.', 'geolocation': '38.911339 -77.007809'},
+               {'name': '515, 6th Street Southeast, Capitol Hill, Washington, D.C.', 'geolocation': '38.882214 -76.998539'},
+               {'name': '3653, 13th Street Northwest, Columbia Heights, Washington, D.C.', 'geolocation': '38.935845 -77.029696'},
+               {'name': '600, Independence Avenue Southwest, Penn Quarter, Washington, D.C.', 'geolocation': '38.887485 -77.020065'},
+               {'name': '3103, Channing Street Northeast, Gateway, Arboretum, Washington, D.C.', 'geolocation': '38.922024 -76.963348'},
+               {'name': '3018, Dumbarton Street Northwest, Georgetown, Washington, D.C.', 'geolocation': '38.907348 -77.059972'}]
