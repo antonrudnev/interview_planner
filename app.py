3d548f2786863c46f3ba230c21d287ad4e9cf200
Anton Rudnev <anton.rudnev@gmail.com>
2018-12-28 05:33:06 -0500

    Show first address element if house number not available (with pop)

--- app.py
+++ app.py
@@ -67,10 +67,10 @@ def geocoding(query):
         address = location['address']
         if address['state'] not in ['D.C.', 'Maryland', 'Virginia']:
             raise Exception('Only DC, MD, and VA are supported now')
-        display_address = ', '.join(p for p in [address.get('house_number', next(iter(address.values()))),
-                                                address.get('road', address.get('pedestrian')),
-                                                address.get('city', address.get('locality')),
-                                                address.get('state')] if p)
+        display_address = ', '.join(p for p in [address.pop('house_number', address.pop(next(iter(address.keys())))),
+                                                address.pop('road', address.pop('pedestrian')),
+                                                address.pop('city', address.pop('locality')),
+                                                address.pop('state')] if p)
         return jsonify({'lat': lat, 'lon': lon, 'address': display_address})
     except:
         abort(404)
