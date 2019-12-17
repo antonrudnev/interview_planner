a60e7597f7bb45d61bbdb4822b0015edb7132931
Anton Rudnev <anton.rudnev@gmail.com>
2019-12-17 18:02:13 -0500

    Updated dependencies.

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-MAP_SERVER_HOST = 'http://54.175.11.175:9966'
-TRIP_SERVER_HOST = 'http://54.175.11.175:5050'
+MAP_SERVER_HOST = 'http://localhost:9966'
+TRIP_SERVER_HOST = 'http://localhost:5000'
 
 TEST_POINTS = [{'name': '1000, Thomas Jefferson St NW, Georgetown, Washington, D.C.', 'geolocation': '38.902815 -77.060595'},
                {'name': '4703, 7th Street Northeast, Michigan Park, Washington, D.C.', 'geolocation': '38.946460 -76.996822'},
